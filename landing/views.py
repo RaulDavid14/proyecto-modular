e1800dd97d9de5f1c0f1c6e07aaeae58833595c3
Montoya2000 <dialbertomm@hotmail.com>
2024-10-15 21:58:00 -0600

    Landing agregada
    Primera Version BASICA de landing Page

--- landing/views.py
+++ landing/views.py
@@ -1,4 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 def index(request):
-    return render(request, 'landing.html')
+    return render(request, 'landing/index.html')
+
+def login_view(request):
+    if request.method == 'POST':
+        # Lógica para manejar el inicio de sesión
+        return redirect('index')
+    return render(request, 'landing/index.html')
+
+def register_view(request):
+    if request.method == 'POST':
+        # Lógica para manejar el registro
+        return redirect('index')
+    return render(request, 'landing/index.html')
