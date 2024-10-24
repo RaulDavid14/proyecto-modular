c5932ea7adc8d60b408359e632cc640ec736829e
raul6712 <rauldavidc14@gmail.com>
2024-10-24 17:16:17 -0600

    Se sube la version inicial del la opción "Datos generales" del formulario.

--- catalogos/views.py
+++ catalogos/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
-
+from .forms import *
+from datos_socioeconomicos.forms import *
 # Create your views here.
 def index(request):
-    return render(request, 'datosgenerales.html')
+    datos = {
+        'form' : DatosGeneralesForm(),
+        'economico' : DatosSocioeconomicosForm()
+    }
+
+    return render(request, 'datosgenerales.html', datos)
