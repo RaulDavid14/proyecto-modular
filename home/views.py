2d8689985dc737852f5ef18c01d81ea87ce96d67
Raul David <raul.david@amentum.net>
2024-12-06 18:11:28 -0600

    cambios pendientes

--- home/views.py
+++ home/views.py
@@ -2,31 +2,22 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import *
 from django.contrib.auth.decorators import login_required
 from catalogos.models import CatCuestionarios
+from django.views.decorators.cache import cache_control
 
 @login_required
+@cache_control(no_store=True, no_cache=True, must_revalidate=True)
 def index(request):
     nombre = request.user.first_name
     apellido = request.user.last_name
 
-    cuestionarios = [
-        'Leche y productos lacteos',
-        'Carnes y productos de origen animal',
-        'Verduras y Hortalizas',
-        'Frutas y frutos Secos',
-        'Leguminosas y cereales',
-        'productos de panadería',
-        'Miceláneos',
-        'Bebidas',
-        'Platillos preparados'
-    ]
-    
+    cuestionarios = [cuestionario.nombre_largo for cuestionario in CatCuestionarios.objects.all()]
+
     opciones = """
-        <button class="btn btn-primary" >Iniciar</button>
-        <button class="btn btn-warning" >Reiniciar</button>
+        <button class="btn btn-outline-success w-50" >Iniciar</button>
+        <button class="btn btn-outline-warning w-25" >Reiniciar</button>
      """
 
     datos = {
-        'nombre' : f'{nombre} {apellido}',
         'opciones' : opciones,
         'cuestionarios' : cuestionarios
     }
