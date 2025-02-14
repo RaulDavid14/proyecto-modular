08978f45fd7ee7a78c2c42b520b257e57b07a63e
Raul David <raul.david@amentum.net>
2025-02-14 14:27:59 -0600

    Se agrega panel de administrador
    Se crean las vistas iniciales para modificar la administracion del cuestionario

--- panel_administrador/urls.py
+++ panel_administrador/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path 
-
+from .views import *
 
 urlpatterns = [
-    
+    path('', index, name='dasahboard_admin'),
+    path('cuestionarios/', index_cuestionario, name = 'dashboard_cuestionario'),
+    path('cuestionarios/editar/<str:cuestionario>', editar_cuestionario, name = 'dashboard_editar_cuestionario'),
+    path('cuestionarios/eliminar/<str:cuestionario>', eliminar_cuestionario, name = 'dashboard_eliminar_cuestionario'),
 ]
--- panel_administrador/views.py
+++ panel_administrador/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.paginator import Paginator
+from catalogos.models import CatCuestionarios
+from cuestionario.models import PreguntaModel
+from django.urls import reverse
 
-# Create your views here.
+# muestra el las estadisticas de los usuarios que se registraron dentro del panel de administrador.
+def index(request):
+    return render(request, 'dashboard/index.html')
+
+# muestra la lista de cuestionarios que aparecen en el cuestionario
+def index_cuestionario(request):
+    cuestionarios = []
+    for cuestionario in CatCuestionarios.objects.all():
+        editar_cuestionario = reverse('dashboard_editar_cuestionario', args=[cuestionario.abreviacion])
+        eliminar_cuestionario = reverse('dashboard_eliminar_cuestionario', args=[cuestionario.abreviacion])
+        acciones = f"""
+            <a class="btn btn-outline-success" href="{editar_cuestionario}">Editar</a>
+            <a class="btn btn-outline-danger" href="{eliminar_cuestionario}">Eliminar</a>
+        """
+        cuestionarios.append({
+            'acciones': acciones,
+            'nombre': cuestionario.nombre_largo
+        })
+    
+    return render(request, 'dashboard/cuestionario_list/index.html', context = {'cuestionarios': cuestionarios})
+
+def editar_cuestionario(request, cuestionario):
+    cuestionario = CatCuestionarios.objects.get(abreviacion = cuestionario)
+    return render(request, 'dashboard/cuestionario_edit/index.html')
+
+
+def eliminar_cuestionario(request, cuestionario):
+    cuestionario = CatCuestionarios.objects.get(abreviacion = cuestionario)
+    return redirect('dashboard_cuestionario')
