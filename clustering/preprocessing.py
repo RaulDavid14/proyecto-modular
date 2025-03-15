a12db00989c62d79f75d4c5b51ce44cdb178f1e9
Lesem Medrano <ingenieria.info7@gmail.com>
2025-03-15 15:33:53 -0600

    cambios clustering

--- clustering/preprocessing.py
+++ clustering/preprocessing.py
@@ -3,7 +3,7 @@ from sklearn.preprocessing import StandardScaler, MinMaxScaler
 
 def preprocesar_datos(datos):
     if not datos:
-        print("❌ No hay datos para preprocesar")
+        print(" No hay datos para preprocesar")
         return np.array([])
 
     # Encuentra la longitud de la lista más larga
@@ -11,15 +11,13 @@ def preprocesar_datos(datos):
 
     # Rellenar las listas más cortas con ceros (o cualquier otro valor que tenga sentido)
     for i in range(len(datos)):
-        if len(datos[i]) < max_length:
-            datos[i].extend([0] * (max_length - len(datos[i])))  # Rellenar con ceros
-        elif len(datos[i]) > max_length:
+        if len(datos[i]) > max_length:
             datos[i] = datos[i][:max_length]  # Recortar si la lista es más larga
 
     datos_np = np.array(datos)
-    print("📊 Datos después de hacerlos homogéneos:", datos_np)
+    print("Datos después de hacerlos homogéneos:", datos_np)
 
-    print("📊 Datos antes de escalar:", datos_np)
+    print("Datos antes de escalar:", datos_np)
 
     # Elegir el tipo de escalado dependiendo de la cantidad de datos
     if datos_np.shape[0] > 1:
@@ -29,7 +27,7 @@ def preprocesar_datos(datos):
 
     # Escalar los datos
     datos_escalados = scaler.fit_transform(datos_np)
-    print("📊 Datos escalados:", datos_escalados)
+    print("Datos escalados:", datos_escalados)
 
     return datos_escalados
 
@@ -44,8 +42,6 @@ def limpiar_datos(datos):
     # Eliminar filas con valores NaN o infinitos
     datos_np = datos_np[~np.isnan(datos_np).any(axis=1)]
     datos_np = datos_np[~np.isinf(datos_np).any(axis=1)]
-
-
 
     print("Datos después de la limpieza:", datos_np)
 
