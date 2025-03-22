a47a327da605c02664f4cbad6a8d49da1f54c32e
Raul David <raul.david@amentum.net>
2025-03-21 21:53:59 -0600

    se borran imagenes y se modifica form
    se borran las imagenes del proyecto y se actualiza el apartado para capturar los datos generales del usuario

--- home/views.py
+++ home/views.py
@@ -2,8 +2,17 @@ from django.shortcuts import render
 from django.contrib.auth import *
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.cache import cache_control
+
 from utils.cuestionario import Cuestionario
 from utils.progreso_sm import ProgresoStateMachine as ProgresoSM
+from datos_socioeconomicos.models import RespuestasDatosgenerales as Respuestas
+from catalogos.models import (
+    CatIngresos
+    ,CatNivelEducativo
+    ,CatPoblacion
+    ,CatSexo
+    ,CatSituacionLaboral
+)
 
 @login_required
 @cache_control(no_store=True, no_cache=True, must_revalidate=True)
@@ -26,6 +35,46 @@ def informe_nutricional(request):
     return render(request, 'informe.html', data)
 
 
+@login_required
+@cache_control(no_store=True, no_cache=True, must_revalidate=True)
+def datos_generales(request):
+    respuestas = Respuestas.objects.filter(usuario=request.user.id).first()  
+
+    if request.method == 'POST':
+        situacion_laboral = request.POST.get('situacionlaboral')
+        ingresos = request.POST.get('ingresos')
+        poblacion = request.POST.get('poblacion')
+        sexo = request.POST.get('sexo')
+        nivel_educativo = request.POST.get('niveleducativo')
+
+        if respuestas:
+            respuestas.situacion_laboral = situacion_laboral
+            respuestas.ingresos = ingresos
+            respuestas.poblacion = poblacion
+            respuestas.sexo = sexo
+            respuestas.nivel_educativo = nivel_educativo
+        else:
+            respuestas = Respuestas.objects.create(
+                usuario=request.user.id,
+                situacion_laboral=situacion_laboral,
+                ingresos=ingresos,
+                poblacion=poblacion,
+                sexo=sexo,
+                nivel_educativo=nivel_educativo
+            )
+        respuestas.save()
+
+    datos = {
+        'respuestas': respuestas,  
+        'sexo': CatSexo.objects.all(),
+        'situacion_laboral': CatSituacionLaboral.objects.all(),
+        'nivel_educativo': CatNivelEducativo.objects.all(),
+        'ingresos': CatIngresos.objects.all(),
+        'poblacion': CatPoblacion.objects.all(),
+        'nombre': request.user.nombre_completo
+    }
+
+    return render(request, 'datos_generales.html', datos)
 
 @login_required
 @cache_control(no_store=True, no_cache=True, must_revalidate=True)
